91ddbe95c8a4a5b864ef5ce3037aea513c97a826
Mipodi <juliamjohansson@gmail.com>
2022-03-16 14:42:20 +0100

    Adds working groq fetch from sanity content lake and renders on the front page

---
+++ web/_data/frontPage.js
@@ -0,0 +1,48 @@
+const BlocksToMarkdown = require("@sanity/block-content-to-markdown");
+const groq = require("groq");
+const client = require("../utils/sanityClient.js");
+const serializers = require("../utils/serializers");
+const overlayDrafts = require("../utils/overlayDrafts");
+const hasToken = !!client.config().token;
+
+function generatePage(sitePage) {
+  return {
+    ...sitePage,
+    excerpt: BlocksToMarkdown(sitePage.excerpt, { serializers, ...client.config() }),
+    body: BlocksToMarkdown(sitePage.body, { serializers, ...client.config() }),
+  };
+}
+
+async function getFrontPage() {
+  // Learn more: https://www.sanity.io/docs/data-store/how-queries-work
+  const filter = groq`*[_type == "sitePage" && _id == "d5363a3d-2e16-470e-9306-7ff90d049f66" && publishedAt < now()]`;
+  const projection = groq`{
+    _id,
+    publishedAt,
+    title,
+    slug,
+    excerpt,
+    mainImage,
+    body[]{
+      ...,
+      children[]{
+        ...,
+        // Join inline reference
+        _type == "authorReference" => {
+          // check /studio/documents/authors.js for more fields
+          "name": @.author->name,
+          "slug": @.author->slug
+        }
+      }
+    },
+    "authors": authors[].author->
+  }`;
+  const order = `| order(publishedAt asc)`;
+  const query = [filter, projection, order].join(" ");
+  const docs = await client.fetch(query).catch((err) => console.error(err));
+  const reducedDocs = overlayDrafts(hasToken, docs);
+  const preparePages = reducedDocs.map(generatePage);
+  return preparePages;
+}
+
+module.exports = getFrontPage;
